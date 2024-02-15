af5780e00f7e77e81b3011e56e038109526e7bf7
JasnaFaisal <you@example.com>
2024-02-15 19:00:01 +0300

    model training

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name="DiamonPricePrediction2",
+    name="StudentPerformanceIndicator",
     version="0.0.1",
     author="Jasna",
     author_email="jasnaovungal@gmail.com",
